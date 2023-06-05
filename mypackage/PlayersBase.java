700305f339452711d03b130f7ce0b209ae478d39
Faputa123 <129268334+Faputa123@users.noreply.github.com>
2023-06-05 12:54:04 +0300

    LB7

---
+++ mypackage/PlayersBase.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class PlayersBase {
+    
+    private static final String[] PLAYERS = new String[] {
+        "Бачира", "Мегуми", "Баал", "Гин", "Ренске", "Юдай", "Ватару", "Хёма", "Исаги", "Окухито"
+    };
+
+    public static String[] getPlayers() {
+        return PLAYERS;
+    }
+}
---
+++ mypackage/Starter.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class Starter {
+    
+    public static void main(String[] args) {
+        
+        new PlayerChooserFrame().setVisible(true); 
+    }
+}
